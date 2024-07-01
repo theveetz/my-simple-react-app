5207fc1bd7756d4be1033d2af40fb36b42b49145
Nikolina <nikkipersonal2@gmail.com>
2024-07-01 19:01:42 +0930

    Clean install dependencies and update component imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import Footer from './components/Footer';
+import { Header, SearchBar, Footer } from './components';
 
 function App() {
   return (
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+// src/components/index.js
+export { default as Header } from './Header';
+export { default as SearchBar } from './SearchBar';
+export { default as Footer } from './Footer';
