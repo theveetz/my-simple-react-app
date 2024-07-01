bb5f229c92096381898cf340566fd76f1d326d2f
Nikolina <nikkipersonal2@gmail.com>
2024-07-01 18:53:52 +0930

    Updated App.js content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My Simple React App</h1>
-        <p>This is a simple React app for showcasing my skills as the veetz developer.</p>
-      </header>
+      <Header />
+      <SearchBar />
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
