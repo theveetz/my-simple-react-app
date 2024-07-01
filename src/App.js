1718c8390ccc8a52a4fad014bd5ffb31f149c680
Nikolina <nikkipersonal2@gmail.com>
2024-07-01 18:41:00 +0930

    Updated App.js content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My Simple React App</h1>
+        <p>This is a simple React app for showcasing my skills as the veetz developer.</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
